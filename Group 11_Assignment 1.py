ca4e60806567cae63dc6a09abfcb20a79dec2e44
Dstrawman <112821137+Dstrawman@users.noreply.github.com>
2022-09-04 18:13:53 -0400

    Add files via upload
    Consolidated the first 3 parts here, working on part 4 tonight but wanted to upload what I had for now.

---
+++ Group 11_Assignment 1.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  4 13:28:39 2022
+
+@author: DStrawma
+"""
+#Request Library
+import requests as r
+from bs4 import BeautifulSoup
+import csv
+import time as t
+import random as rnd
+import pandas as pd
+
+#scrape variables
+url = 'http://drd.ba.ttu.edu/isqs6339/assign/assign_1/'
+fp = 'C:\\Users\\dstrawma\\Downloads\\'
+filename = 'assignemnt_1.csv'
+lowval = 5
+highval = 7
+
+#aseembling the list of url we will need for the entire assignent.
+res1 = r.get(url)
+soup = BeautifulSoup(res1.content, 'lxml')
+
+#singles out the href links that leads to further details for the three phones
+pager = soup.find('div', attrs={'id' : 'phonelist'}).find_all('a')
+
+#creating a list with the original list and then use a for loop to append the additional urls from pager
+urllist = [url]
+for p in pager:
+    urllist.append(url + p['href']) 
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""         
+                          Task 1: Print Web Server and Web Call Info
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+print('Values for Task #1')
+
+#Check HTML Status Code
+print('Status Code of Call:  ',res1.status_code)
+
+#Check for page redirecting
+print('Page is Redirecting:  ',res1.is_redirect)
+
+#Return current page encoding
+print('Current Page Encoding:  ',res1.encoding)
+
+#Returns server response headers
+print('Returned Header for Page:  ',res1.headers)
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""         
+                          Task 2: Print iPhone Details
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+#using find to single out the area of code that has the details for the three phones
+phone_details = soup.find('div', attrs={'id' : 'phonelist'}).find_all('li')  
+
+#indexing to get required iPhone details
+iphone_os = phone_details[6].text
+iphone_color = phone_details[5].text
+print('***Task #2')
+print('The following are values for the iPhone 11 Pro')
+print(iphone_os)
+print(iphone_color)
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""         
+                 Task 3: Extract storage & camera features
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+#from what I can see we can hardcode this task
+iphone_url = 'http://drd.ba.ttu.edu/isqs6339/assign/assign_1/phone.php?id=1341'
+res3 = r.get(iphone_url)
+soup3 = BeautifulSoup(res3.content, 'lxml') 
+
+#getting storage 
+iphone_storage_code = soup3.find('div',attrs={'id':'Phoneinitial'}).find_all('td')
+iphone_storage = iphone_storage_code[2].text.rstrip() +' '+ iphone_storage_code[3].text.rstrip()
+
+#getting front camera features
+front_camera_code = soup3.find('div',attrs={'id':'Phoneotherstuff'}).find_all('span',attrs={'class':'tag'})
+front_camera_features = soup3.find('div',attrs={'id':'Phoneotherstuff'}).find('ul').find_all('li')
+#printing
+print('***Task #3***')
+print(iphone_storage)
+print('Front Camera Features: ')
+for li in front_camera_features:
+    print(li.text)
+
+
+
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""         
+                 Task 4: Export data to csv
+"""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+
+
+
+
+
+
+
+
+
+
+
+
